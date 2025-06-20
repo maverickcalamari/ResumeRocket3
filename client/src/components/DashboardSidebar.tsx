40281fb8c6fe6f70d8b2b1295bf5342e938038b1
maverickcalamari <trey.d.brown982@gmail.com>
2025-06-19 20:42:25 -0400

    Update DashboardSidebar.tsx

--- client/src/components/DashboardSidebar.tsx
+++ client/src/components/DashboardSidebar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";Add commentMore actions
+import { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
