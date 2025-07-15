dfe92a5a1595c17fe9feae088bf07070c5664a36
maverickcalamari <trey.d.brown982@gmail.com>
2025-07-15 16:19:17 -0400

    Updated openai.ts

--- server/openai.ts
+++ server/openai.ts
@@ -1,5 +1,7 @@
 import OpenAI from "openai";
 import { ResumeAnalysis, ResumeSuggestion, SkillGap, EmploymentGap, ResumeTemplate, SectionAnalysis, CompetitiveAnalysis } from "@shared/schema";
+import dotenv from 'dotenv';
+dotenv.config();
 
 // the newest OpenAI model is "gpt-4o" which was released May 13, 2024. do not change this unless explicitly requested by the user
 const openai = new OpenAI({
